289a9bf3a400a4a74765867cafc91f6aa0fc3e8f
Florante Gamat <yonggamat@gmail.com>
2022-05-08 20:05:14 +0800

    Initial Commit: Setup Spring Boot Application

---
+++ src/test/java/com/github/yonggamat/springcloudstream/SpringCloudStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yonggamat.springcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
